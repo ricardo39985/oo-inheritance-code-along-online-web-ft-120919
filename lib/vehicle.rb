b7bee4b13ba21c80aed88a5388218b68af6ea9ec
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 22:14:14 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
+  attr_reader :wheel_size, wheel_number
+  @@all = []
+  def initialize(size, number)
+    @wheel_size = size
+    @wheel_number = number
+    @@all << self
+  end
+  def go
+    'vrrrrrrrooom!'
+  end
+  def fill_up_tank
+    'filling up!'
+  end
 
 end
